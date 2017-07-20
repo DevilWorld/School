f3dc8acc8ad711a8966b55111ee8ebf4db14acf2
dinesh.venkatachalam <dinesh.venkatachalam@DEP-WS1K91022>
2017-07-20 18:23:31 -0400

    Added loginviewmodel

---
+++ Web Applications/SchoolPortal/SchoolPortal/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolPortal.Models
+{
+    public class LoginViewModel
+    {
+        public string UserId { get; set; }
+        public string Password { get; set; }
+    }
+}
